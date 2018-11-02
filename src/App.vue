1d37af43c44f40cf5903e0c5bf13e4a0ba0ddc78
Younghoon Jung <allinux36@gmail.com>
2018-11-03 02:52:46 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <HelloWorld cccc="2222fgf"/>
+    <my/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import my from "./components/my";
+
+export default {
+  name: "App2",
+  components: {
+    HelloWorld,
+    my
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: cursive, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/my.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div>
+      {{ name }} {{ name2 }}
+    </div>
+    <div>
+      <mu-button class="but" color="blue">One</mu-button>
+    </div>
+  </div>
+</template>
+
+<script>
+var f = () => {
+  return [...Array(10).keys()].reduce((a, b) => a + b);
+};
+export default {
+  name: "my",
+  data() {
+    return {
+      name: (() => {
+        return [...Array(10).keys()].reduce((a, b) => a + b);
+      })(),
+      name2: f()
+    };
+  }
+};
+</script>
+
+<style scoped>
+div {
+  font-family: "monospace";
+  font-size: 30px;
+}
+.but {
+  width: 300px;
+  text-transform: none;
+}
+</style>
